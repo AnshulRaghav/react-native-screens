53790e23bb1d7c71b7971cc94dcc240521369dfa
AnshulRaghav <anshulraghav2222@gmail.com>
2021-09-19 02:40:41 +0530

    sm fic

--- src/Components/ChatPrice.js
+++ src/Components/ChatPrice.js
@@ -6,7 +6,7 @@ export default class ChatPrice extends Component {
         return (
             <View style={{flex:1,backgroundColor:'#ffffff',alignItems:'center'}}>
                 <Image source={require('../Avatar/pr.png')} style={{width:300}}/>
-                <View style={{flex:.7,backgroundColor:'#F8F8F8',padding:16,borderRadius:12,margin:16}}>
+                <View style={{backgroundColor:'#F8F8F8',padding:16,borderRadius:12,margin:16}}>
                     <Text style={{fontSize:18,fontWeight:'bold'}}>Price Rule</Text>
                     <Text style={{paddingTop:8,paddingBottom:8,color:'grey'}}>1. If you need help with your React Native app, the right place to go depends on the type of help that you need.</Text>
                     <Text style={{paddingTop:8,paddingBottom:8,color:'grey'}}>2. If you need help with your React Native app, the right place to go depends on the type of help that you need.</Text>
