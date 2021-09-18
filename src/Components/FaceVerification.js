36eaeba570406a59e7cd793053135a8f30e3bb3b
AnshulRaghav <anshulraghav2222@gmail.com>
2021-09-18 23:52:39 +0530

    styles fix

--- src/Components/FaceVerification.js
+++ src/Components/FaceVerification.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export default class FaceVerification extends Component {
     render() {
         return (
-            <View style={{backgroundColor:'#ffffff',flex:1,alignItems:'center',justifyContent:'space-between'}}>
-                {/* <View style={{flex:.8,backgroundColor:'red'}}> */}
-                    <Image 
-                    source={require('../Avatar/face.png')} 
-                    style={{height:400,width:300,marginTop:48}}
-                    />
-                {/* </View> */}
-                <TouchableOpacity style={{backgroundColor:'#CA16FE',padding:16,paddingLeft:32,paddingRight:32,borderRadius:24,marginBottom:16,width:'80%'}}>
-                    <Text style={{fontWeight:'bold',color:'#ffffff',alignSelf:'center',fontSize:16}}>VERIFY AGAIN</Text>
+            <View style={styles.container}>
+                <Image 
+                source={require('../Avatar/face.png')} 
+                style={styles.img}
+                />
+                <TouchableOpacity style={styles.btn}>
+                    <Text style={styles.btnTxt}>VERIFY AGAIN</Text>
                 </TouchableOpacity>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor:'#ffffff',
+        flex:1,
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    img : {
+        height:400,
+        width:300,
+        marginTop:48
+    },
+    btn :{
+        backgroundColor:'#CA16FE',
+        padding:16,
+        paddingLeft:32,
+        paddingRight:32,
+        borderRadius:24,
+        marginBottom:16,
+        width:'80%'
+    },
+    btnTxt : {
+        fontWeight:'bold',
+        color:'#ffffff',
+        alignSelf:'center',
+        fontSize:16
+    }
+})
